e8f110245a2b3fcef81a326dba4b0f21dddadd15
Rohit Yadav <rk4740779@gmail.com>
2025-10-17 10:29:49 +0100

    updated readme

--- server.py
+++ server.py
@@ -3,45 +3,45 @@ from fastmcp import FastMCP
 mcp = FastMCP("MCP Calculator Server")
 
 @mcp.tool()
-def add(a:int, b:int) -> int:
+async def add(a:int, b:int) -> int:
     return a+b
 
 
 @mcp.tool()
-def subtract(a:int, b:int) -> int:
+async def subtract(a:int, b:int) -> int:
     return a - b
 
 
 @mcp.tool()
-def multiply(a:int, b:int) -> int:
+async def multiply(a:int, b:int) -> int:
     return a * b
 
 
 @mcp.tool()
-def divide(a:float, b:float) -> float:
+async def divide(a:float, b:float) -> float:
     if b == 0:
         raise ValueError("Division by zero is not allowed")
     return a / b
 
 
 @mcp.tool()
-def modulus(a:int, b:int) -> int:
+async def modulus(a:int, b:int) -> int:
     if b == 0:
         raise ValueError("Modulus by zero is not allowed")
     return a % b
 
 
 @mcp.tool()
-def power(a:float, b:float) -> float:
+async def power(a:float, b:float) -> float:
     return a ** b
 
 
 @mcp.tool()
-def sqrt(x:float) -> float:
+async def sqrt(x:float) -> float:
     if x < 0:
         raise ValueError("Square root of a negative number is not allowed")
     return x ** 0.5
 
 
 if __name__ == "__main__":
-    mcp.run()
+    mcp.run(transport="stdio")
